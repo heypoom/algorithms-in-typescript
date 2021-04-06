17b020e965cf312785dc0bba561f75e6b4837326
Phoomparin Mano <poom@hey.com>
2021-04-06 18:41:16 +0700

    test: add tests for linearly linked graphs

--- tests/graph-bfs.test.ts
+++ tests/graph-bfs.test.ts
@@ -1,13 +1,14 @@
-import {createGraph, collect, bfs} from '~/graph'
+import {createGraph} from '~/graph'
 
-describe('Graph Breadth First Search', () => {
-  it('should work', () => {
+describe('Graph Breadth-First Search', () => {
+  it('should handle linearly linked graph', () => {
     const graph = createGraph({A: 1, B: 2, C: 3, D: 4, E: 5, F: 6, G: 7})
     graph.chain('A', 'B', 'C', 'D', 'E', 'F', 'G')
 
-    const edges = graph.collect('A', graph.bfs) //?
-    console.log(graph.log(edges))
+    const edgesA = graph.collect('A', graph.bfs)
+    expect(edgesA.join('')).toBe('BCDEFG')
 
-    const edgez = collect((onVisit) => bfs('A', graph, onVisit)) //?
+    const edgesB = graph.collect('D', graph.bfs)
+    expect(edgesB.join('')).toBe('EFG')
   })
 })
