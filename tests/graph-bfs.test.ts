5b734a7a25b5d305144a925508589f3ccbd911be
Phoomparin Mano <poom@hey.com>
2021-04-06 19:40:42 +0700

    feat: add tests for utility functions variant

--- tests/graph-bfs.test.ts
+++ tests/graph-bfs.test.ts
@@ -1,7 +1,9 @@
-import {createGraph} from '~/graph'
+import {bfs, createGraph} from '~/graph'
+
+import {collect} from '~/utils'
 
 describe('Graph Breadth-First Search', () => {
-  it('should handle linearly linked graph', () => {
+  it('should handle linearly linked graph with class methods', () => {
     const graph = createGraph({A: 1, B: 2, C: 3, D: 4, E: 5, F: 6, G: 7})
     graph.chain('A', 'B', 'C', 'D', 'E', 'F', 'G')
 
@@ -9,6 +11,17 @@ describe('Graph Breadth-First Search', () => {
     expect(edgesA.join('')).toBe('BCDEFG')
 
     const edgesB = graph.collect('D', graph.bfs) //?
+    expect(edgesB.join('')).toBe('EFG')
+  })
+
+  it('should handle linearly linked graph with utility functions', () => {
+    const graph = createGraph({A: 1, B: 2, C: 3, D: 4, E: 5, F: 6, G: 7})
+    graph.chain('A', 'B', 'C', 'D', 'E', 'F', 'G')
+
+    const edgesA = collect((onVisit) => bfs('A', graph, onVisit)) //?
+    expect(edgesA.join('')).toBe('BCDEFG')
+
+    const edgesB = collect((onVisit) => bfs('D', graph, onVisit)) //?
     expect(edgesB.join('')).toBe('EFG')
   })
 })
