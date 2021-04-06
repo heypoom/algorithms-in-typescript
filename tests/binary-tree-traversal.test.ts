345e383e0eeaf853fc02549093b84ab768f41269
Phoomparin Mano <poom@hey.com>
2021-04-06 18:36:29 +0700

    test: add binary tree traversal unit tests

--- tests/binary-tree-traversal.test.ts
+++ tests/binary-tree-traversal.test.ts
@@ -1,22 +1,40 @@
 import {N, collect, bfs, dfss, dfs, Order, traverse, Algo} from '~/binary-tree'
 
-//    A
-//  B   C
-// D E F G
-
-const tree = N('A', N('B', N('D'), N('E')), N('C', N('F'), N('G')))
+/**
+       A
+     B   C
+    D E F G
+  */
+const createTree = () => N('A', N('B', N('D'), N('E')), N('C', N('F'), N('G')))
 
 describe('Binary Tree Traversal', () => {
-  it('should work', () => {
-    collect(tree, bfs).join('') //?
-    collect(tree, dfss).join('') //?
+  it('should perform breadth-first search', () => {
+    const nodes = collect(createTree(), bfs)
 
-    collect(tree, dfs, Order.Pre).join('') //?
-    collect(tree, dfs, Order.In).join('') //?
-    collect(tree, dfs, Order.Post).join('') //?
+    expect(nodes.join('')).toBe('ABCDEFG')
+  })
 
-    bfs(tree, (v) => console.log(v), Order.In)
-    dfs(tree, (v) => console.log(v), Order.In)
-    dfss(tree, (v) => console.log(v))
+  it('should perform pre-order depth-first search', () => {
+    const nodes = collect(createTree(), dfs, Order.Pre)
+
+    expect(nodes.join('')).toBe('ABDECFG')
+  })
+
+  it('should perform in-order depth-first search', () => {
+    const nodes = collect(createTree(), dfs, Order.In)
+
+    expect(nodes.join('')).toBe('DBEAFCG')
+  })
+
+  it('should perform post-order depth-first search', () => {
+    const nodes = collect(createTree(), dfs, Order.Post)
+
+    expect(nodes.join('')).toBe('DEBFGCA')
+  })
+
+  it('should perform in-order depth-first search using stack', () => {
+    const nodes = collect(createTree(), dfss)
+
+    expect(nodes.join('')).toBe('DBEAFCG')
   })
 })
