4c334985804c71e4d5cee3398ce59ba8bb32184b
Phoomparin Mano <poom@hey.com>
2021-04-07 03:04:16 +0700

    test: change test case for cycling graphs

--- tests/graph-bfs.test.ts
+++ tests/graph-bfs.test.ts
@@ -35,7 +35,7 @@ describe('Complex Graph Breadth-First Search', () => {
   it('should be able to search cyclic graphs', () => {
     const graph = createGraph({A: 1, B: 2, C: 3})
     graph.chain('A', 'C')
-    graph.chain('C', 'B', 'C', 'A')
+    graph.chain('C', 'A', 'B')
 
     const edges = graph.collect('A', graph.bfs) //?
     expect(edges.join('')).toBe('ACB')
