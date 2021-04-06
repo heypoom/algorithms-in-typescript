71812a7c506acd69c41373e9d139e02b1d5b34c9
Phoomparin Mano <poom@hey.com>
2021-04-06 18:38:23 +0700

    test: refactor tests into multiple suites

--- tests/binary-tree-traversal.test.ts
+++ tests/binary-tree-traversal.test.ts
@@ -7,13 +7,15 @@ import {N, collect, bfs, dfss, dfs, Order, traverse, Algo} from '~/binary-tree'
   */
 const createTree = () => N('A', N('B', N('D'), N('E')), N('C', N('F'), N('G')))
 
-describe('Binary Tree Traversal', () => {
+describe('Binary Tree Breadth-First Search', () => {
   it('should perform breadth-first search', () => {
     const nodes = collect(createTree(), bfs)
 
     expect(nodes.join('')).toBe('ABCDEFG')
   })
+})
 
+describe('Binary Tree Depth-First Search', () => {
   it('should perform pre-order depth-first search', () => {
     const nodes = collect(createTree(), dfs, Order.Pre)
 
@@ -32,7 +34,7 @@ describe('Binary Tree Traversal', () => {
     expect(nodes.join('')).toBe('DEBFGCA')
   })
 
-  it('should perform in-order depth-first search using stack', () => {
+  it('should perform in-order depth-first search with stack', () => {
     const nodes = collect(createTree(), dfss)
 
     expect(nodes.join('')).toBe('DBEAFCG')
