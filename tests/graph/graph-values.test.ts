6dccd88ea21786dbf35fb2d67d3caac4df834493
Phoomparin Mano <poom@hey.com>
2021-04-09 02:47:34 +0700

    docs: add comments on graph value test

--- tests/graph/graph-values.test.ts
+++ tests/graph/graph-values.test.ts
@@ -10,12 +10,14 @@ describe('Graph Values', () => {
     expect(g.valueOf('D')).toBe(40)
   })
 
-  it('can update graph values using add and setValues', () => {
+  it('can update graph values using set and setValues', () => {
     const g = createGraph({A: 0, B: 0, C: 0, D: 0})
 
+    // Set one value at a time.
     g.set('A', 1)
     g.set('B', 2)
 
+    // Set multiple values at a time.
     g.setValues({C: 3, D: 4})
 
     expect(g.valueOf('A')).toBe(1)
